faa1515fd58b7f6e017dbd9ff6d3167ad0b03e43
tharindu-sandaruwan <tharindusandaruwan011@gmail.com>
2025-06-02 14:40:35 +0530

    completed update delete methods

--- server/routes/MovieRoutes.js
+++ server/routes/MovieRoutes.js
@@ -1,12 +1,12 @@
 const express =require ("express");
 const router=express.Router();
-const Task=require("../models/Movies");
+const Movie=require("../models/Movies");
 
 router.post("/",async(req, res)=>{
     try{
-        const task =new Task(req.body);
-        await task.save();
-        res.status(201).json(task);
+        const movie =new Movie(req.body);
+        await movie.save();
+        res.status(201).json({message:"Movie added Successfully!"})
     }catch(err){
         res.status(400).json({error:err.message})
     }
@@ -14,10 +14,29 @@ router.post("/",async(req, res)=>{
 
 router.get("/",async(req,res)=>{
     try{
-        const task=await Task.find();
-        res.json(task);
+        const movie=await Movie.find();
+        res.json(movie);
     }catch(err){
         res.status(500).json({error:err.message})
     }
 })
+
+router.put("/:id",async(req,res)=>{
+    try{
+        const movie=await Movie.findByIdAndUpdate(req.params.id,req.body,{new:true});
+        res.json(movie)
+    }catch(err){
+        res.status(400).json({error:err.message});
+    }
+})
+
+router.delete("/:id",async (req, res)=>{
+    try{
+        await Movie.findByIdAndDelete(req.params.id);
+        res.json({message:"Movie Deleted Successfully!"});
+    }catch(err){
+        res.status(400).json({error:err.message});
+    }
+})
+
 module.exports =router
